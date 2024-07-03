98afaab1ca6680494e51458cb5763677d013c1a9
Michael <141164270+Skelligg@users.noreply.github.com>
2024-07-03 22:18:00 +0100

    fixed localstorage issues, added table and now adding custom food

--- js/index-script.js
+++ js/index-script.js
@@ -11,28 +11,24 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 getFoodsFromLocalStorage();
 
 async function getFoodsFromLocalStorage(){
-    let order = [];
-    let keys = [];
-
-    for(let i = 0; i < localStorage.length; i++){
-        order[i] = localStorage.getItem(localStorage.key(i));
-        keys[i] = localStorage.key(i);
-    }
+    let keys = Object.keys(localStorage);
+    let order = getValuesFromLocalStorageInOrder();
 
     for(let i = 0; i < order.length; i++){
-        let index = Number(order[i].slice(-1));
-        let value = order[i].slice(0,-1)
+
+        let values = order[i].split('-');
 
         let id = Number(keys[i].slice(-1))
+        
         if( id > addedFoodID){
             addedFoodID = id
         }
-        fetchSearchResults(value, index, "stored")
-        await delay(500);
+        fetchNutrients(values[0], values[1], values[2], "stored")
     }
 }
 
 let result = [];
+let idResult = [];
 let data;
 let nutrientData;
 
@@ -40,6 +36,8 @@ let buttons = document.querySelectorAll(".close")
 let wrappers = document.querySelectorAll(".wrapper")
 const search = document.querySelector(".search-bar")
 const resultBox = document.querySelector(".result-box")
+const showTableButton = document.querySelector(".show-table")
+const downloadable = document.querySelector(".downloadable")
 
 init()
 
@@ -52,8 +50,13 @@ function init(){
     }
     document.querySelector("body").addEventListener("click", () => resultBox.innerHTML = "")
     document.addEventListener('DOMContentLoaded', calculateTotal);
+    showTableButton.addEventListener("click", toggleTable)
 }
 
+    function toggleTable() {
+        (this.innerHTML == "Show Table") ? this.innerHTML = "Hide Table" : this.innerHTML = "Show Table";
+        (downloadable.style.display == "none") ? downloadable.style.display = "block" : downloadable.style.display = "none";
+    }
 
 function removeWrapper(e){
     let targetWrapperID = e.target.getAttribute("for")
@@ -88,25 +91,62 @@ function searchResults(e){
     if(e.key === "Enter"){
         let foodItem = e.target.value;
         fetchSearchResults(foodItem)
-
     }
 }
 
-async function fetchNutrients(fullName, searchInput, index, lS){
-    const nutrientURL = "https://api.edamam.com/api/food-database/v2/nutrients?app_id=e0854d27&app_key=ecc8bdb05ddbd3dbe828f0ee73c8c791";
+async function fetchSearchResults(foodItem){
+    const parserURL = `https://api.edamam.com/api/food-database/v2/parser?app_id=${app_id}&app_key=${app_key}&ingr=${foodItem}`;
+    try{
+        const response = await fetch(parserURL)
 
-    const parserURL = `https://api.edamam.com/api/food-database/v2/parser?app_id=${app_id}&app_key=${app_key}&ingr=${fullName}`;
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+          }
+
+        data = await response.json();
+
+        result = [];
+        idResult = [];
+
+        if(foodItem){
+            for(let i = 0; i < 10 && i < data.hints.length; i++){
+                const obj = {
+                    name: data.hints[i].food.label,
+                    id : data.hints[i].food.foodId,
+                    measureURI : data.hints[i].measures[0].uri
+                }
+                idResult[i] = obj;
+                result[i] = data.hints[i].food.label;
+                
+            }
+        }
+        const content = result.map((list) =>{
+            return "<li>" + list + "</li>"
+        });
+        (foodItem) ? content.push("<li><strong>Add Custom Food..</strong></li>") : null;
+
+
+        resultBox.innerHTML = "<ul>" + content.join('') + "</ul>"
     
-    const response = await fetch(parserURL)
-    if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
-      }
+        let searchResults = document.querySelectorAll("li");
+        for(let i = 0; i < searchResults.length; i++){
+            if(searchResults[i].innerHTML != "<strong>Add Custom Food..</strong>"){
+                searchResults[i].addEventListener("click",function() { 
+                    fetchNutrients(idResult[i].name, idResult[i].id, idResult[i].measureURI)})
+            }
+            else{
+                searchResults[i].addEventListener("click", function () {
+                    addCustomFood(foodItem)})
+            }
+        }    
+    }
+    catch(error){
+        console.error(error)
+    }
+}
 
-    data = await response.json();
-
-    let foodName = data.hints[index].food.label;
-    let foodID = data.hints[index].food.foodId;
-    let measureURI = data.hints[index].measures[0].uri;
+async function fetchNutrients(name, APIfoodID, measureURI, lS){
+    const nutrientURL = "https://api.edamam.com/api/food-database/v2/nutrients?app_id=e0854d27&app_key=ecc8bdb05ddbd3dbe828f0ee73c8c791";
 
     try{
         const response = await fetch(nutrientURL, {
@@ -122,7 +162,7 @@ async function fetchNutrients(fullName, searchInput, index, lS){
                         "qualifiers": [
                           "https://api.edamam.com/api/food-database/v2/nutrients"
                         ],
-                        "foodId": `${foodID}`
+                        "foodId": `${APIfoodID}`
                       }
                     ]
                   })
@@ -135,57 +175,15 @@ async function fetchNutrients(fullName, searchInput, index, lS){
           }
 
         nutrientData = await response.json();
-        addFood(foodName, searchInput, index, lS);
+        addFood(name, APIfoodID, measureURI, lS);
 
     }
     catch(error){
         console.log(error)
     }
-    
-    
 }
 
-async function fetchSearchResults(foodItem, index, lS){
-    const parserURL = `https://api.edamam.com/api/food-database/v2/parser?app_id=${app_id}&app_key=${app_key}&ingr=${foodItem}`;
-    try{
-        const response = await fetch(parserURL)
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-          }
-
-        data = await response.json();
-
-        result = [];
-
-        if(foodItem){
-            for(let i = 0; i < 10 && i < data.hints.length; i++){
-                result[i] = data.hints[i].food.label;
-            }
-        }
-        if(lS){
-            fetchNutrients(result[index], foodItem, index, lS)
-        }
-        else{
-            const content = result.map((list) =>{
-                return "<li>" + list + "</li>"
-            });
-        
-            resultBox.innerHTML = "<ul>" + content.join('') + "</ul>"
-    
-            let searchResults = document.querySelectorAll("li");
-            for(let i = 0; i < searchResults.length; i++){
-                searchResults[i].addEventListener("click",function() { fetchNutrients(search.value, foodItem, i)})
-            }
-        }    
-    }
-    catch(error){
-        console.error(error)
-    }
-
-}
-
-function addFood(name, searchInput, index, lS){
+function addFood(name, APIfoodID, measureURI, lS){
 
     let weight = nutrientData?.totalWeight ?? 0;
     let caloriesPer100g = Number((nutrientData?.totalNutrients?.ENERC_KCAL?.quantity?.toFixed(2) ?? 0) / weight * 100).toFixed(0);
@@ -202,9 +200,8 @@ function addFood(name, searchInput, index, lS){
     const wrapper = document.createElement('div');
     wrapper.className = 'wrapper';
     if(lS){
-        let compareValue = searchInput + index
         for (let i = 0; i < localStorage.length; i++) {
-            if(localStorage.getItem(localStorage.key(i)) == compareValue){
+            if(localStorage.getItem(localStorage.key(i)) == `${name}-${APIfoodID}-${measureURI}`){
                 wrapper.id = `${localStorage.key(i)}`;
                 newFoodID = `${localStorage.key(i)}`;
             }
@@ -258,7 +255,7 @@ function addFood(name, searchInput, index, lS){
     amountInput.value = 100;
 
     const pGrams = document.createElement('p');
-    pGrams.innerHTML = "<p>g</p>"
+    pGrams.innerHTML = "g"
 
     // Append the h3 and amount input to the weight div
     weightDiv.appendChild(nutritionHeader);
@@ -288,15 +285,16 @@ function addFood(name, searchInput, index, lS){
     }
 
     // Create the cost input
+    const costP = document.createElement('p');
+    costP.innerHTML = 'Cost: '
+
     const costInput = document.createElement('input');
     costInput.type = 'number';
     costInput.className = 'cost';
-    costInput.placeholder = 'cost';
-    costInput.min = '0';
-    costInput.max = '9999';
 
     // Append the cost input to the nutrition div
-    nutritionDiv.appendChild(costInput);
+    costP.appendChild(costInput)
+    nutritionDiv.appendChild(costP);
 
     // Append the weight div and nutrition div to the collapsible-text div
     collapsibleText.appendChild(weightDiv);
@@ -322,6 +320,29 @@ function addFood(name, searchInput, index, lS){
     // Append the wrapper div to the body or a specific container element
     addedFoodsDiv.appendChild(wrapper);
 
+    const foodTableRow = document.createElement('tr');
+    foodTableRow.id = `food-row-${newFoodID}`;
+    const rowData = [
+        name,
+        `${caloriesPer100g} kCal`,
+        `${amountInput.value} g`,
+        `${carbsPer100g} g`,
+        `${fiberPer100g} g`,
+        `${totalSugarsPer100g} g`,
+        `${energyPer100g} kJ`,
+        `${fatPer100g} g`,
+        `${proteinPer100g} g`,
+        `0.00`
+    ];
+    
+    rowData.forEach(data => {
+        const td = document.createElement('td');
+        td.innerHTML = data;
+        foodTableRow.appendChild(td);
+    });
+
+    document.getElementById('food-table-body').appendChild(foodTableRow);
+
     // Update nutrients based on the amount input value
     amountInput.addEventListener('input', () => {
         const amount = amountInput.value;
@@ -332,13 +353,33 @@ function addFood(name, searchInput, index, lS){
         nutrientElements.fat.innerHTML = `Fat: <strong>${(fatPer100g * amount / 100).toFixed(2)} g</strong>`;
         nutrientElements.protein.innerHTML = `Protein: <strong>${(proteinPer100g * amount / 100).toFixed(2)} g</strong>`;
         label.innerHTML = `${name} <p>${(caloriesPer100g * amount / 100).toFixed(0)} kCal</p><img src="./resources/arrow.png">`;
+        
+        const foodTableRow = document.getElementById(`food-row-${newFoodID}`);
+        foodTableRow.children[1].innerText = `${(caloriesPer100g * amount / 100).toFixed(0)} kCal`;
+        foodTableRow.children[2].innerText = `${amountInput.value} g`;
+        foodTableRow.children[3].innerHTML = `${(carbsPer100g * amount / 100).toFixed(2)} g`;
+        foodTableRow.children[4].innerHTML = `${(fiberPer100g * amount / 100).toFixed(2)} g`;
+        foodTableRow.children[5].innerHTML = `${(totalSugarsPer100g * amount / 100).toFixed(2)} g`;
+        foodTableRow.children[6].innerHTML = `${(energyPer100g * amount / 100).toFixed(2)} kJ`;
+        foodTableRow.children[7].innerHTML = `${(fatPer100g * amount / 100).toFixed(2)} g`;
+        foodTableRow.children[8].innerHTML = `${(proteinPer100g * amount / 100).toFixed(2)} g`;
+
         calculateTotal();
     });
 
-    costInput.addEventListener('input', calculateTotal);
+    costInput.addEventListener('input', () => {
+        const foodTableRow = document.getElementById(`food-row-${newFoodID}`);
+        foodTableRow.children[9].innerText = `${costInput.value} `;
+        calculateTotal();
+    });
+
+    closeButton.addEventListener("click", () => {
+        document.getElementById(`food-row-${newFoodID}`).remove();
+        calculateTotal();
+    });
 
     if(!lS){
-        localStorage.setItem(`wrapper-${addedFoodID}`, searchInput + index);
+        localStorage.setItem(`wrapper-${addedFoodID}`, `${name}-${APIfoodID}-${measureURI}`);
         addedFoodID++;
     }
     
@@ -403,6 +444,17 @@ function calculateTotal(){
     document.getElementById('total-protein').innerHTML = `Protein: <strong>${totalProtein.toFixed(2)} g</strong>`;
     document.getElementById('total-weight').innerHTML = `Weight: <strong>${totalWeight.toFixed(2)} g</strong>`;
     document.getElementById('total-cost').innerHTML = `Cost: <strong>${totalCost.toFixed(2)}</strong>`;
+
+    const totalRow = document.getElementById('total-row');
+    totalRow.children[1].innerText = `${totalCalories.toFixed(0)} kCal`;
+    totalRow.children[2].innerText = `${totalWeight.toFixed(2)} g`;
+    totalRow.children[3].innerHTML = `${totalCarbs.toFixed(2)} g`;
+    totalRow.children[4].innerHTML = `${totalFiber.toFixed(2)} g`;
+    totalRow.children[5].innerHTML = `${totalSugars.toFixed(2)} g`;
+    totalRow.children[6].innerHTML = `${totalEnergy.toFixed(2)} kJ`;
+    totalRow.children[7].innerHTML = `${totalFat.toFixed(2)} g`;
+    totalRow.children[8].innerHTML = `${totalProtein.toFixed(2)} g`;
+    totalRow.children[9].innerHTML = `${totalCost.toFixed(2)}`;
 }
 
 
@@ -454,6 +506,8 @@ $('.darkmode-checkbox').addEventListener('click', () => {
         t.classList.toggle('darkerblue', !isDarkmode);
         t.classList.toggle('lighterblue', isDarkmode);
     });
+
+    (isDarkmode) ? showTableButton.style.color = "#c6c6c6" : showTableButton.style.color = "#0000009c"
 });
 
 
@@ -471,4 +525,162 @@ if (removeTransforms) {
     ray.setAttribute('d', `M${p0.x} ${p0.y} L${p1.x} ${p1.y}`);
     
   });  
+}
+
+function getValuesFromLocalStorageInOrder() {
+    let valuesArray = [];
+
+    // Retrieve all keys from localStorage
+    let keys = Object.keys(localStorage);
+
+    // Filter keys to get only those starting with 'wrapper-'
+    let numericKeys = keys.filter(key => key.startsWith('wrapper-'))
+                          .map(key => Number(key.split('-')[1]));
+
+    // Sort the numeric keys
+    numericKeys.sort((a, b) => a - b);
+
+    // Retrieve the values using the sorted keys and store them in an array
+    for (let key of numericKeys) {
+        valuesArray.push(localStorage.getItem('wrapper-' + key));
+    }
+
+    return valuesArray;
+}
+
+function addCustomFood(name){
+    let newFoodID = "";
+
+    // Create the wrapper div
+    const wrapper = document.createElement('div');
+    wrapper.className = 'wrapper';
+
+    if(localStorage.getItem(`wrapper-${addedFoodID}`) != null){
+        while(localStorage.getItem(`wrapper-${addedFoodID}`) != null){
+            addedFoodID++;
+        }
+        wrapper.id = `wrapper-${addedFoodID}`; 
+        newFoodID = `${addedFoodID}`   
+    }
+    else{
+        wrapper.id = `wrapper-${addedFoodID}`; 
+        newFoodID = `${addedFoodID}`  
+    }
+
+    // Create the collapsible div
+    const collapsible = document.createElement('div');
+    collapsible.className = 'collapsible';
+
+    // Create the input checkbox
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = `food-${newFoodID}`;
+    checkbox.style.display = "none";
+
+    // Create the label
+    const label = document.createElement('label');
+    label.setAttribute('for', `food-${newFoodID}`);
+    label.innerHTML = `${shortenName(name)} <p>${caloriesPer100g} kCal</p><img src="./resources/arrow.png">`;
+
+    // Create the collapsible-text div
+    const collapsibleText = document.createElement('div');
+    collapsibleText.className = 'collapsible-text';
+
+    // Create the weight div
+    const weightDiv = document.createElement('div');
+    weightDiv.className = 'weight';
+
+    // Create the h3 element
+    const nutritionHeader = document.createElement('h3');
+    nutritionHeader.textContent = 'Nutrition';
+
+    // Create the amount input
+    const amountInput = document.createElement('input');
+    amountInput.type = 'number';
+    amountInput.className = 'amount';
+    amountInput.value = 100;
+
+    const pGrams = document.createElement('p');
+    pGrams.innerHTML = "g"
+
+    // Append the h3 and amount input to the weight div
+    weightDiv.appendChild(nutritionHeader);
+    weightDiv.appendChild(amountInput);
+    weightDiv.appendChild(pGrams);
+
+    // Create the nutrition div
+    const nutritionDiv = document.createElement('div');
+    nutritionDiv.className = 'nutrition';
+
+    // Create the p elements for nutrition facts
+    const nutritionFacts = {
+        carbs: `Carbs: <strong>${carbsPer100g} g</strong>`,
+        fiber: `Fiber: <strong>${fiberPer100g} g</strong>`,
+        totalSugars: `Sugar: <strong>${totalSugarsPer100g} g</strong>`,
+        energy: `Energy: <strong>${energyPer100g} kJ</strong>`,
+        fat: `Fat: <strong>${fatPer100g} g</strong>`,
+        protein: `Protein: <strong>${proteinPer100g} g</strong>`
+    };
+
+    const nutrientElements = {};
+    for (const [key, fact] of Object.entries(nutritionFacts)) {
+        const p = document.createElement('p');
+        p.innerHTML = fact;
+        nutritionDiv.appendChild(p);
+        nutrientElements[key] = p;
+    }
+
+    // Create the cost input
+    const costP = document.createElement('p');
+    costP.innerHTML = 'Cost: <input type="number" class="cost" >'
+
+    // Append the cost input to the nutrition div
+    nutritionDiv.appendChild(costP);
+
+    // Append the weight div and nutrition div to the collapsible-text div
+    collapsibleText.appendChild(weightDiv);
+    collapsibleText.appendChild(nutritionDiv);
+
+    // Append the input, label, and collapsible-text to the collapsible div
+    collapsible.appendChild(checkbox);
+    collapsible.appendChild(label);
+    collapsible.appendChild(collapsibleText);
+
+    // Create the close button
+    const closeButton = document.createElement('button');
+    closeButton.className = 'close';
+    closeButton.innerHTML = 'x';
+    (lS) ? closeButton.setAttribute("for", `${newFoodID}`) : closeButton.setAttribute("for", `wrapper-${newFoodID}`);
+    
+    closeButton.addEventListener("click", removeWrapper);
+
+    // Append the collapsible div and close button to the wrapper div
+    wrapper.appendChild(collapsible);
+    wrapper.appendChild(closeButton);
+
+    // Append the wrapper div to the body or a specific container element
+    addedFoodsDiv.appendChild(wrapper);
+
+    const foodTableRow = document.createElement('tr');
+    foodTableRow.id = `food-row-${newFoodID}`;
+    const rowData = [
+        name,
+        `${caloriesPer100g} kCal`,
+        `${amountInput.value} g`,
+        `${carbsPer100g} g`,
+        `${fiberPer100g} g`,
+        `${totalSugarsPer100g} g`,
+        `${energyPer100g} kJ`,
+        `${fatPer100g} g`,
+        `${proteinPer100g} g`,
+        `0.00`
+    ];
+    
+    rowData.forEach(data => {
+        const td = document.createElement('td');
+        td.innerHTML = data;
+        foodTableRow.appendChild(td);
+    });
+
+    document.getElementById('food-table-body').appendChild(foodTableRow);
 }
